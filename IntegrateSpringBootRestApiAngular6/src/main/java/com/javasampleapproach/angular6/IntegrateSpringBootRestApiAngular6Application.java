69da61a7bb9ca343811c20483b14c9365cc8f1d2
Ashish Patil <ashish091992@gmail.com>
2019-01-05 15:15:58 +0530

    Commit

---
+++ IntegrateSpringBootRestApiAngular6/src/main/java/com/javasampleapproach/angular6/IntegrateSpringBootRestApiAngular6Application.java
@@ -0,0 +1,12 @@
+package com.javasampleapproach.angular6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrateSpringBootRestApiAngular6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrateSpringBootRestApiAngular6Application.class, args);
+	}
+}
---
+++ IntegrateSpringBootRestApiAngular6/src/main/java/com/javasampleapproach/angular6/restapi/RestApiController.java
@@ -0,0 +1,13 @@
+package com.javasampleapproach.angular6.restapi;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+ 
+@RestController
+public class RestApiController {
+ 
+	@RequestMapping("/api/hi")
+	public String hi() {
+		return "Hello world! >>> Message from <a href='http://javasampleapproach.com' target='_blank'>JavaSampleApproach.com</a>";
+	}
+}
